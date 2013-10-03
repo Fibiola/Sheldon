a152acfcd6b4bcec112372f3e1052bef9409ed53
Fibiola <natasha.pristovsek@gmail.com>
2013-10-03 03:43:58 +0200

    bug fixes to the simple rock paper scissors game

--- game.js
+++ game.js
@@ -10,37 +10,40 @@ var userChoice = prompt("So, what will you pick? Rock, paper or scissors?");
 
 
 //b.) We want the computer to make it's choice randomly. Math.random() will equal a number between 0 and 1.
-var computerChoice = Math.random();
-console.log(computerChoice);
+computerChoice = Math.random();
 
 if (computerChoice < 0.33) {
     computerChoice = "rock";
-} else if (0.33 < computerChoice < 0.66) {
+} else if (computerChoice > 0.66) {
     computerChoice = "paper";
 } else {
-    computerChoice = "scissors";
-}
+    computerChoice = "scissors"
+};
+
+
 
 // Function that takes two parameters - if both pick the same thing you get a tie message
 var compare = function (choice1, choice2) {
-    if (choice1 === choice2) {
+    if (choice1 == choice2) {
         return "The result is a tie!";
     }
 // if choice 1 = rock and choice 2 equals paper -> paper wins.
-    if (choice1 === "rock") {
-        if (choice2 === "paper") {
+    if (choice1 == "rock") {
+        if (choice2 == "paper") {
             return "Paper covers rock and wins!";
-        } else if (choice2 === "scissors") {
+        } else if (choice2 == "scissors") {
             return "Rock beats scissors and wins!";
     }
 }
-    if (choice1 === "paper") {
-        if (choice2 === "rock") {
+    if (choice1 == "paper") {
+        if (choice2 == "rock") {
             return "Paper covers rock and wins!";
-        } else if (choice2 === "scissors") {
+        } else if (choice2 == "scissors") {
             return "Scissors cut the paper up and win!";
         }
-    }
+    };
 };
 
-console.log(compare(userChoice, computerChoice));
+document.write(compare(userChoice, computerChoice) + "!" + " ");
+document.write("Computer chose" + " " + computerChoice + "!" + " ");
+document.write("You chose" + " " + userChoice + "!" + " ");
